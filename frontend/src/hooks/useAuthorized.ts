2db9c6865e6dd7e5f650e9246c88839273f0e743
mogaya <mogayainnocent@gmail.com>
2025-04-07 21:55:29 +0300

    constants and use authorize

--- frontend/src/hooks/useAuthorized.ts
+++ frontend/src/hooks/useAuthorized.ts
@@ -1,7 +1,8 @@
-import { ACCESS_TOKEN } from "@/constants/constants";
+import { ACCESS_TOKEN, REFRESH_TOKEN } from "@/constants/constants";
 import { useEffect, useState } from "react";
 import { jwtDecode } from "jwt-decode";
 import { CanceledError } from "axios";
+import { api } from "@/services/api-client";
 
 interface DecodedToken {
   exp: number;
@@ -12,42 +13,42 @@ const useAuthorized = () => {
 
   useEffect(() => {
     const controller = new AbortController();
-    // const signal = controller.signal;
+    const signal = controller.signal;
 
     const token = localStorage.getItem(ACCESS_TOKEN);
     if (!token) {
       setIsAuthorized(false);
       return;
     }
-    // const decoded: DecodedToken = jwtDecode(token);
-    // const tokenExp = decoded.exp;
-    // const now = Date.now() / 1000;
+    const decoded: DecodedToken = jwtDecode(token);
+    const tokenExp = decoded.exp;
+    const now = Date.now() / 1000;
 
-    // if (tokenExp < now) {
-    //   const refreshToken = localStorage.getItem(REFRESH_TOKEN);
-    //   if (!refreshToken) {
-    //     setIsAuthorized(false);
-    //     return;
-    //   }
+    if (tokenExp < now) {
+      const refreshToken = localStorage.getItem(REFRESH_TOKEN);
+      if (!refreshToken) {
+        setIsAuthorized(false);
+        return;
+      }
 
-    //   api
-    //     .post("/token/refresh/", { refresh: refreshToken }, { signal })
-    //     .then((res) => {
-    //       if (res.status === 200) {
-    //         localStorage.setItem(ACCESS_TOKEN, res.data.access);
-    //         setIsAuthorized(true);
-    //       } else {
-    //         setIsAuthorized(false);
-    //       }
-    //     })
-    //     .catch((err) => {
-    //       if (err instanceof CanceledError) return;
-    //       console.error("Token Refresh Failed", err);
-    //       setIsAuthorized(false);
-    //     });
-    // } else {
-    //   setIsAuthorized(true);
-    // }
+      api
+        .post("/token/refresh/", { refresh: refreshToken }, { signal })
+        .then((res) => {
+          if (res.status === 200) {
+            localStorage.setItem(ACCESS_TOKEN, res.data.access);
+            setIsAuthorized(true);
+          } else {
+            setIsAuthorized(false);
+          }
+        })
+        .catch((err) => {
+          if (err instanceof CanceledError) return;
+          console.error("Token Refresh Failed", err);
+          setIsAuthorized(false);
+        });
+    } else {
+      setIsAuthorized(true);
+    }
 
     return () => controller.abort();
   }, []);
