e8bdff0b9d8bb7915de4983bca3fc893362ee2fe
mogaya <mogayainnocent@gmail.com>
2025-04-09 23:58:25 +0300

    rm pesapal copy

--- PesaPal-main copy/pin.php
+++
@@ -1,7 +0,0 @@
-<?php
-header("Content-Type: application/json");
-$pinCallbackResponse = file_get_contents('php://input');
-$logFile = "pin.json";
-$log = fopen($logFile, "a");
-fwrite($log, $pinCallbackResponse);
-fclose($log);
